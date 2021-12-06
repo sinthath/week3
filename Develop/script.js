2220eb2151ccd8f93e390c009df28c63dd9fa1f7
sinthath <sinthathirath@gmail.com>
2021-12-05 21:12:12 -0500

    update to function

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,28 @@
 // Assignment code here
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector(".btn");
+var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = '01234567890';
+var special = '~!@#$%^&*()_+{}[]|\:";<>?,./';
+var minLength = 8;
+var maxLength = 128;
 
 // Write password to the #password input
 function writePassword() {
-  var passwordLength = window.prompt('Enter password length, it must be at least 8 characters and no more than 128 characters.');
-  var useLower = window.prompt('Do you wish to include lower case characters? Enter "YES" or "NO to choose")');
-  var useUpper = window.prompt('Do you wish to include uppercase characters? Enter "YES" or "NO to choose")');
-  var useNumeric = window.prompt('Do you wish to include numeric characters? Enter "YES" or "NO to choose")');
-  var useSpecial = window.prompt('Do you wish to include special characters? Enter "YES" or "NO to choose")');
 
-  var password = generatePassword() {
-    var min = 8;
-    var max = 128;
-    var alphaLower = "abcdefghijklmnopqrstuvwxyz";
-    var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var num = '01234567890';
-    var special = '~!@#$%^&*()_+{}[]|\:";<>?,./';
+  function generatePassword() {
+    var passwordLength = window.prompt('Enter password length, it must be at least 8 characters and no more than 128 characters.');
+    var useLower = window.confirm('Do you wish to include lower case characters?');
+    var useUpper = window.confirm('Do you wish to include uppercase characters?');
+    var useNumeric = window.confirm('Do you wish to include numeric characters?');
+    var useSpecial = window.confirm('Do you wish to include special characters?');
+
+    let validChar = '';
+
+    if (passwordLength < minLength || passwordLength > maxLength ) {
+      return false
+      ;
+    }
 
     if (useLower) {
       validChar += alphaLower;
@@ -36,22 +40,23 @@ function writePassword() {
       validChar += special;
     }
 
-    let pass = '';
+    let password = '';
 
     for (let i = 0; i < passwordLength; i++) {
-      pass += validChar[Math.floor(Math.random() * (validChar.length))]
+      password += validChar[Math.floor(Math.random() * (validChar.length))]
     }
 
-    return pass
-
-  };
+    return password
+  }
+  
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
 
-  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+document.getElementById("generate").addEventListener("click", writePassword);
+
+
